2707cac8fe999a6f2e867dd92dcfa50b222152e1
Aarush_009 <aarushkumarchoubey@gmail.com>
2022-07-20 16:57:54 +0530

    Add files via upload

--- seeds/index.js
+++ seeds/index.js
@@ -1,1 +1,50 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
 
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author: '62b9c7df7b00b9f1d5664bc8',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/dgon34qvt/image/upload/v1657032333/YelpCamp/az4tpcfp5xxqc6erdghm.avif',
+                    filename: 'YelpCamp/az4tpcfp5xxqc6erdghm'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dgon34qvt/image/upload/v1657032333/YelpCamp/vmtshuswwxwtsljp9mfe.avif',
+                    filename: 'YelpCamp/vmtshuswwxwtsljp9mfe'
+                }
+            ]
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
